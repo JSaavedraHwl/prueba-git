9575596cd250fde47e931405369f54001a9100a7
JSaavedraHwl <164583281+JSaavedraHwl@users.noreply.github.com>
2024-10-26 11:04:59 -0300

    Se agrega peticion http a componente home y agrega html.

--- src/app/consumo-api.service.ts
+++ src/app/consumo-api.service.ts
@@ -1,6 +1,39 @@
 import { Injectable } from '@angular/core';
 import { inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+export type Root = Root2[]
+
+export interface Root2 {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: Address
+  phone: string
+  website: string
+  company: Company
+}
+
+export interface Address {
+  street: string
+  suite: string
+  city: string
+  zipcode: string
+  geo: Geo
+}
+
+export interface Geo {
+  lat: string
+  lng: string
+}
+
+export interface Company {
+  name: string
+  catchPhrase: string
+  bs: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,6 +42,7 @@ export class ConsumoApiService {
   constructor() { }
 
   getApi(){
-    return this.http.get('https://jsonplaceholder.typicode.com/users');
+    return this.http.get<Root>('https://jsonplaceholder.typicode.com/users');
   }
 }
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ConsumoApiService, Root } from '../consumo-api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  consumoApiService = inject(ConsumoApiService);
 
+  datos: Root = [];
   constructor() {}
+
+  ngOnInit(): void {
+    this.consumoApiService.getApi().subscribe((data) => {
+      console.log(data);
+      this.datos = data;
+    });
+  }
 
 }
